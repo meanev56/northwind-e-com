0541a4d44ad56d3f511b71c44509386a0b76c570
meanev56 <emekanjoku86@gmail.com>
2026-06-17 11:13:13 +0100

    Implement Clerk authentication and webhook handling; update environment configurations and dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,27 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerk";
+import { getEnv } from "./lib/env";
+
+const env = getEnv();
 const app = express();
 
-app.listen(3001, () => console.log("Server is running on port 3001"));
+const rawJson = express.raw({ type: "application/json", limit: "1mb" });
+
+// it's important that you don't parse the webhook event data, it should be in the raw format
+app.post("/webhooks/clerk", rawJson, (req, res) => {
+  void clerkWebhookHandler(req, res);
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+
+
+
+
+app.listen(env.PORT, () => console.log("Server is running on port:", env.PORT));
